8a013816b62ab1518bc5f530d4fe8bea40730629
Sorin Chis <kish.sorin@yahoo.com>
2019-06-24 12:06:25 +0300

    added  scripts for migration and seeds for roles and users

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test_kitchenDb.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
