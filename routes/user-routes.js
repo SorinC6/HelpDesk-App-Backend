4a901022b5b0b2189a9caee415d7005858cfad40
Sorin Chis <kish.sorin@yahoo.com>
2019-06-25 13:14:27 +0300

    added middleware for checking user type

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const responseStatus = require("../config/responseStatuses");
+const restricted = require("../middleware/restrictedRoute");
+const validRole = require("../middleware/roleCheck");
 
 const dbHelpers = require("../models/userHelper");
 
-router.get("/users", async (req, res, next) => {
+router.get("/users", restricted, validRole([3]), async (req, res, next) => {
   try {
-    const user = await dbHelpers.getAllUsers();
+    const users = await dbHelpers.getAllUsers();
     res.status(responseStatus.successful).json({ users });
   } catch (error) {
     next(responseStatus.serverError);
